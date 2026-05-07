9be1aa384c6fcbb815af9d3b834a1e6c26fa7f87
Tharun <Tharun.g5311@gmail.com>
2026-05-07 00:40:13 -0400

    new init files and the api bridge that connects the web files to the src files

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import webview
+from src.core.config import WEB_DIR, APP_NAME, DEBUG
+from src.api import NocturneAPI
+
+def main():
+    api = NocturneAPI()
+
+    window = webview.create_window(
+        title=APP_NAME,
+        url=str(WEB_DIR / "index.html"),
+        js_api=api,
+        width=1000,
+        height=700,
+        resizable=True,
+        background_color='#1a1a1a'
+    )
+
+    api.set_window(window)
+
+    webview.start(debug=DEBUG)
+
+if __name__ == "__main__":
+    main()
